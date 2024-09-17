7381adbcec959d21ebe70cf6fa88abe06b400ed6
nikulindev <etci3200@gmail.com>
2024-09-17 21:09:33 +0200

    RA-04 Create first fragment with categories

--- app/src/main/java/com/nikulindev/recipesapp/MainActivity.kt
+++ app/src/main/java/com/nikulindev/recipesapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nikulindev.recipesapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import com.nikulindev.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val fragment = CategoriesListFragment()
+
+            val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(R.id.mainContainer, fragment)
+
+            fragmentTransaction.commit()
+        }
     }
 }
